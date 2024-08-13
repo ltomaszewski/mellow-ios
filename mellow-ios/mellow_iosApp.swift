90452a48e73789cfdeec35e92de9e0567fcfae86
Lucas Tomaszewski <luki22@gmail.com>
2024-08-13 17:55:11 +0200

    Initial Commit

---
+++ mellow-ios/mellow_iosApp.swift
@@ -0,0 +1,32 @@
+//
+//  mellow_iosApp.swift
+//  mellow-ios
+//
+//  Created by Lukasz Tomaszewski on 13/08/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct mellow_iosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}

5b92ea71c807f37ceddf9b866bec29cda693a82b
Lucas Tomaszewski <luki22@gmail.com>
2024-08-22 09:08:05 +0200

    Added: Better sync on pick date from top bar and then move to calendar day view

--- mellow-ios/HomeTabBarView/Tabs/Components/CalendarDayView/CalendarDayViewWithPager.swift
+++ mellow-ios/HomeTabBarView/Tabs/Components/CalendarDayView/CalendarDayViewWithPager.swift
@@ -13,13 +13,13 @@ struct CalendarDayViewWithPager: View {
     
     // TODO: If date has changed via tab on date bar the pageViewContent has to scroll to that view
     var body: some View {
-        PageViewContent<CalendarDayView, Date>(startIndex: date,
-                                               getCurrentIndex: { $0.viewModel.midDayDate },
-                                               nextIndex: { $0.adjustDay(by: 1) },
-                                               previousIndex: { $0.adjustDay(by: -1) },
-                                               viewBuilder: { CalendarDayView(date: .constant($0), databaseStore: databaseStore) },
-                                               hasNextPage: { true },
-                                               hasPreviousPage: { true },
-                                               indexHasChanged: { date = $0 })
+        PageViewContent(index: $date,
+                        getCurrentIndex: { $0.viewModel.midDayDate },
+                        nextIndex: { $0.adjustDay(by: 1) },
+                        previousIndex: { $0.adjustDay(by: -1) },
+                        viewBuilder: { CalendarDayView(date: .constant($0), databaseStore: databaseStore) },
+                        hasNextPage: { true },
+                        hasPreviousPage: { true },
+                        indexHasChanged: { date = $0 })
     }
 }

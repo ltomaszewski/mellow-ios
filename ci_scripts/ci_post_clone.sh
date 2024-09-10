1a7d84f89568294b654de056cee219ddaad9ff2d
Lucas Tomaszewski <luki22@gmail.com>
2024-09-10 14:19:51 +0200

    Fixed: CI and macros support

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,11 @@
+//
+//  ci_post_clone.sh
+//  mellow-ios
+//
+//  Created by Lukasz Tomaszewski on 10/09/2024.
+//
+
+#!/bin/zsh
+
+mkdir -p ~/Library/org.swift.swiftpm/security/
+cp macros.json ~/Library/org.swift.swiftpm/security/

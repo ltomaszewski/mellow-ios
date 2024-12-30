1a214e217d25e5d7be627372eb7692807c9f8a0a
Lucas Tomaszewski <luki22@gmail.com>
2024-12-30 11:43:51 +0100

    Added: Enabled Firebase

--- mellow-ios/mellow_iosApp.swift
+++ mellow-ios/mellow_iosApp.swift
@@ -8,20 +8,22 @@
 import SwiftUI
 import SwiftData
 import FirebaseCore
+import FirebaseAnalytics
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        Analytics.setAnalyticsCollectionEnabled(true)
+        return true
+    }
 }
 
 @main
 struct mellow_iosApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var rAppStateStore = AppState.Store(databaseService: .init())
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
